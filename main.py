2e2c3e6dbeced8e45515f6d9dd180e90c3221705
soonie-doongie-dori <speckintheworld@gmail.com>
2024-02-27 20:37:40 +0300

    adding pepper

--- main.py
+++ main.py
@@ -58,9 +58,5 @@ def print_pizza_to_ui(pizza: AbstractPizza) ->None:
 
 
 if __name__ == '__main__':
-    pizza = BigPizza()
-    pizza = Pepper(pizza)
-    pizza = Pepper(pizza)
-    pizza = Pepper(pizza)
     pizza = Pepper(pizza)
     print_pizza_to_ui(pizza)
